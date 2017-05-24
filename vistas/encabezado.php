f85b9344596ff7a8ee67cdbeb5c8114cc3d30f2f
esteban1234 <tva_libra@hotmail.com>
2017-05-24 10:44:55 -0500

    codigo de verificacion

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="u6vX6OT9SyGSOKmwsV_ggD3sunDjiyXz__lpCo9Uio0" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>Maecvo</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
   	<META NAME="Title" content="Maecvo">
-  	<META NAME="Author" CONTENT="[]"/>
+  	<META NAME="Author" CONTENT="[http://maecvo.com/]"/>
   	<meta name="keywords" content="contabilidad, impuestos, facturación digital, facturación electrónica, soluciones, planes, impuestos, regímenes fiscales">
     <meta name="description" content="Maecvo Tus impuestos y contabilidad online, sin complicaciones.">
   	<link rel="stylesheet" href="css/bootstrap.css">
